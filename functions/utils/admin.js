8e395d900935df92ff76551bf761f5259ca52226
janithsamee <janith.samee0@gmail.com>
2023-03-26 00:46:42 +0530

    fixed update error

--- functions/utils/admin.js
+++ functions/utils/admin.js
@@ -1,7 +1,10 @@
 const admin = require("firebase-admin");
 
+var serviceAccount = require("../serviceAccountKey.json");
+
 admin.initializeApp({
-	credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://eespire-2b2e2-default-rtdb.firebaseio.com",
 });
 const db = admin.firestore();
 
